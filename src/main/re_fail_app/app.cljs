10c7f22118f7dd24b946d9a5e56501bf690b1349
Lasse Määttä <lasse.olavi.maatta@gmail.com>
2023-12-22 09:02:59 +0200

    Instrument function calls

--- src/main/re_fail_app/app.cljs
+++ src/main/re_fail_app/app.cljs
@@ -1,11 +1,16 @@
 (ns re-fail-app.app
-  (:require [re-fail-app.views.core :as views]
-            [reagent.dom :as rd]
-            [re-frame.core :as rf]))
+  (:require [cljs.spec.test.alpha :as stest]
+            [re-fail-app.views.core :as views]
+            [re-frame.core :as rf]
+            [reagent.dom :as rd]))
 
 (enable-console-print!)
 
-(defn render
+(defn- instrument
+  []
+  (println "Instrumented:" (stest/instrument)))
+
+(defn- render
   []
   (rd/render [views/view]
              (.getElementById js/document "root")))
@@ -13,8 +18,10 @@
 (defn ^:dev/after-load clear-cache-and-render!
   []
   (rf/clear-subscription-cache!)
+  (instrument)
   (render))
 
 (defn ^:export init
   []
+  (instrument)
   (render))
